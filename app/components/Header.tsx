36ba95fa1eab8819630a3d3ccdc33b81fbc429f7
AJSolis01 <AJSolis01@outlook.com>
2024-06-11 22:14:42 -0400

    fix invalid <link>

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,28 +2,28 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-zinc-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-2xl font-bold">
           <Link href="/">
-            <a>MyNextApp</a>
+            Austin Solis
           </Link>
         </div>
         <nav>
           <ul className="flex space-x-4">
             <li>
-              <Link href="/">
-                <a className="hover:text-gray-400">Home</a>
+              <Link href="/" className="hover:text-gray-400">
+                Home
               </Link>
             </li>
             <li>
-              <Link href="/about">
-                <a className="hover:text-gray-400">About</a>
+              <Link href="/about" className="hover:text-gray-400">
+                About
               </Link>
             </li>
             <li>
-              <Link href="/contact">
-                <a className="hover:text-gray-400">Contact</a>
+              <Link href="/contact" className="hover:text-gray-400">
+                Contact
               </Link>
             </li>
           </ul>
